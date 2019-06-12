09ded07e88bf1afda8a91ec77d7835d26acc593e
Jackson Meade <jacksonmeade@outlook.com>
2019-06-12 13:49:34 -0400

    Include 3D Types file

--- src/CG_3dTypes.jl
+++ src/CG_3dTypes.jl
@@ -10,5 +10,5 @@ Point3 = Union{Point3C,Point3H}
 
 toPoint3H((x,y,z)::Point3C)::Point3H = (one(x),x,y,z)
 
-dot((ux,uy,uz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat},(vx,vy,vz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat}) = (ux * vx) + (uy * vy) + (uz * vz)
-dot((ux,uy,uz)::Vector3,(vx,vy,vz)::Vector3) = (ux * vx) + (uy * vy) + (uz * vz)
+dot((ux,uy,uz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat},(vx,vy,vz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat}) = muladd(ux, vx, muladd(uy, vy, (uz * vz))
+dot((ux,uy,uz)::Vector3,(vx,vy,vz)::Vector3) = muladd(ux, vx, muladd(uy, vy, (uz * vz))
--- src/CGinJulia.jl
+++ src/CGinJulia.jl
@@ -1,6 +1,6 @@
 module CGinJulia
 #
-export Vector2,Point2, Point2C, Point2H, Line2, Vector3, Point3, Point3C, Point3H, Line3
+export Vector2, Point2, Point2C, Point2H, Line2, Vector3, Point3, Point3C, Point3H, Line3
 export toPoint2H
 export dot, perpdot
 export ccw,leftturn,rightturn
@@ -9,5 +9,6 @@ export leftofseg, rightofseg, leftward, rightward
 export line, orientptln, leftof, rightof
 
 include("CG_2dTypes.jl")
+include("CG_3dTypes.jl")
 
 end  # module CGinJulia
