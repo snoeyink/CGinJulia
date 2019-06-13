a24af78603c28e2a91e2b17604c1bc1425fe4847
Jackson Meade <jacksonmeade@outlook.com>
2019-06-13 15:22:51 -0400

    ...

--- test/CG_3dTypes.jl
+++ test/CG_3dTypes.jl
@@ -1,6 +1,6 @@
 @testset "Basic 3D Orientation Tests" begin
-    w = (0f0,0f0); x = (1,1,2,2); y = (1,2,1,1); z = (1,2,1,2); p = (2,2,2); q = (1,3,4,3); r = (1f0,1f0,1f0,1f0); s = (2,3,4);
+    w = (0f0,0f0); x = (1,2,2); y = (2,1,1); z = (2,1,2); p = (2,2,2); q = (3,4,3); r = (1f0,1f0,1f0); s = (2,3,4);
 
-    @test plane(q,x,y,z)
+    @test plane(r,x,y,z)
 
 end;
