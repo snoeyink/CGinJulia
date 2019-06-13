863a4c61ad35dd60de16945864fd78ecc6bf53ef
Jackson Meade <jacksonmeade@outlook.com>
2019-06-13 11:24:37 -0400

    Added rough determinant function and above/below plane function

--- src/CG_3dTypes.jl
+++ src/CG_3dTypes.jl
@@ -1,10 +1,15 @@
 """
-
+3D Vectors (x, y z), Planes (pw, px, py, pz)
 """
+
+@testset "Basic 3D Orientation Tests" begin
+    w = (0f0,0f0); x = (1,2,2); y = (2,1,1); z = (2,1,2); p = (2,2,2); q = (3,4,3); r = (1f0,1f0,1f0); s = (2,3,4);
+
+end;
 
 Vector3 = Tuple{Real,Real,Real}
 Point3C = Tuple{Real,Real,Real}
-Line3 = Tuple{Real,Real,Real,Real}
+Plane3 = Tuple{Real,Real,Real,Real}
 Point3H = Tuple{Real,Real,Real,Real}
 Point3 = Union{Point3C,Point3H}
 
@@ -12,3 +17,14 @@ toPoint3H((x,y,z)::Point3C)::Point3H = (one(x),x,y,z)
 
 dot((ux,uy,uz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat},(vx,vy,vz)::Tuple{AbstractFloat,AbstractFloat,AbstractFloat}) = muladd(ux, vx, muladd(uy, vy, (uz * vz))
 dot((ux,uy,uz)::Vector3,(vx,vy,vz)::Vector3) = muladd(ux, vx, muladd(uy, vy, (uz * vz))
+
+# cartesian and homogeneous orientation determinants
+det2(i::Tuple{Real,Real,Real,Real})::Real = muladd(i[1],i[3],-muladd(i[2],i[4]))
+det3(p::Point3C,q::Point3C,r::Point3C) = muladd(p[1],det2(q[2],q[3],r[2],r[3]),muladd(p[2],det2(q[1],q[3],r[1],r[3]),p[3]*det2(q[1],q[2],r[1],r[2])))
+
+# computing homogeneous plane equations
+plane(o::Point3C,p::Point3C,q::Point3C,r::Point3C)::Plane3 = (det3(o,p,q,r),,,)
+orientptpl((x,y,z)::Point3C,(pw,px,py,pz)::Plane3) = orientptpl(toPoint3H(x,y,z),(pw,px,py,pz))
+orientptpl((w,x,y,z)::Point3H,(pw,px,py,pz)::Plane3) = muladd(px,x,muladd(py,y,muladd(pz,z,pw)))
+abovePl(q::Point3,p::Plane3)::Bool = orientptpl(q,p)>zero(p[1])
+belowPl(q::Point3,p::Plane3)::Bool = orientptpl(q,p)<zero(p[1])
