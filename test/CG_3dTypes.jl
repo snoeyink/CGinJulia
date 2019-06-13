2a696cc58da55ac91f27a44ec51a5dc43c813397
Jackson Meade <jacksonmeade@outlook.com>
2019-06-13 15:27:48 -0400

    Plane test works

--- test/CG_3dTypes.jl
+++ test/CG_3dTypes.jl
@@ -1,6 +1,6 @@
 @testset "Basic 3D Orientation Tests" begin
     w = (0f0,0f0); x = (1,1,2,2); y = (2,2,1,1); z = (1,2,1,2); p = (2,2,2); q = (3,4,3); r = (3f0,1f0,1f0,1f0); s = (2,3,4);
 
-    @test plane(r,x,y,z)
+    @test plane(r,x,y,z) == (-3, -1.0f0, -5.0f0, 6.0f0)
 
 end;
