b741ea03af4903650506324754dc38cdd1a2a633
Jackson Meade <jacksonmeade@outlook.com>
2019-06-13 14:29:27 -0400

    CGinJulia.jl updated to reflect new functions

--- src/CGinJulia.jl
+++ src/CGinJulia.jl
@@ -1,11 +1,12 @@
 module CGinJulia
 #
 export Vector2, Point2, Point2C, Point2H, Line2
-export Vector3, Point3, Point3C, Point3H, Line3
-export toPoint2H
+export Vector3, Point3, Point3C, Point3H, Plane3
+export toPoint2H, toPoint3H
 export dot, perpdot
 export det2, det3
 export abovePl, belowPl
+export plane
 export ccw,leftturn,rightturn
 export leftAhead, rightAhead, leftOnAhead, rightOnAhead
 export leftofseg, rightofseg, leftward, rightward
